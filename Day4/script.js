5f7a9c5faff9bc2d912544d8f011af80cc6a8e51
Abr009 <bradithya945@gmail.com>
2025-05-03 12:17:40 +0530

    Day 4: Curson effects.

---
+++ Day4/script.js
@@ -0,0 +1,35 @@
+const cursor = document.getElementById('cursor');
+const imgDiv = document.getElementById("image");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.display = "flex";
+
+  gsap.to(cursor, {
+    x: e.x - 10,
+    y: e.y - 10,
+    duration: 1,
+    ease: "back.out(1.7)"
+  })
+});
+
+document.addEventListener("mouseleave", () => {
+  cursor.style.display = "none";
+});
+
+imgDiv.addEventListener("mouseenter", () => {
+  cursor.innerHTML = "View More"
+
+  gsap.to(cursor, {
+    scale: 3.5,
+    backgroundColor: '#ffffffa8'
+  })
+})
+
+imgDiv.addEventListener("mouseleave", () => {
+  cursor.innerHTML = ""
+
+  gsap.to(cursor, {
+    scale: 1,
+    backgroundColor: '#fff'
+  })
+})
